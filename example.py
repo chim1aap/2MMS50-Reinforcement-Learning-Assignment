bad4c34102be6bac2ccfd5518f59965670a99bb3
chim1aap <folkert.k.jansen@gmail.com>
2020-06-06 21:32:15 +0200

    Initial Commit

---
+++ example.py
@@ -0,0 +1,31 @@
+import gym
+
+# Choose an environment from https :// gym . openai . com / envs /# atari
+env = gym.make("Centiped-ram-v0")
+
+print(env.action_space)
+print(env.observation_space)
+
+print("Press Enter to Continue...")
+input()
+
+env.reset()
+
+#Run the Game
+for t in range(1000):
+    env.render()
+
+    #Choose Random Action.
+    action = env.action_space.sample()
+
+    #Take the Action, make an observation from the environment and obtain a reward.
+    observation, reward, done, info = env.step()
+
+    print ( " At time " ,t , " , we obtained reward " , reward , " , and observed : " )
+    print ( observation )
+
+    if done:
+        print ( " Episode finished after {} timesteps " . format ( t +1) )
+        break
+
+env.close()

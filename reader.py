671cc7f006d025768f78248b6f85e749d90458cf
chim1aap <folkert.k.jansen@gmail.com>
2020-06-17 21:19:36 +0200

    update reader to produce images. # Conflicts: #	agents.py

--- reader.py
+++ reader.py
@@ -1,5 +1,12 @@
 import pickle
 import pandas
+from plotnine import * # ggplot package for python.
+import numpy
+
+
+'''
+Load results 
+'''
 k = 10000
 with open("./Qlearning/rewards"+str(k) +".pkl", 'rb') as fp:
             print(fp)
@@ -13,17 +20,40 @@ with open("./tdlearning/rewards"+str(k) +".pkl", 'rb') as fp:
             print(fp)
             tdlearning = pickle.load(fp)
 
+'''
+Print summary
+'''
 print(sum(qlearning))
-
 print(sum(sarsa))
 print(sum(tdlearning))
 
-def summary(qlearning):
-    ql = pandas.DataFrame(qlearning)
-    ql = ql.describe()
-    ql = ql.transpose()
-    print(ql.head() )
+def summary(vector):
+    ql = pandas.DataFrame(vector, columns = {"Reward" } )
+    ql.insert(1, "Run", range(len(vector)))
+    qls = ql.describe()
+    qls = qls.transpose()
+    print(qls.head() )
 
+    return ql
 summary(qlearning)
 summary(sarsa)
 summary(tdlearning)
+'''
+plot some results. 
+'''
+df = pandas.DataFrame(range(len(sarsa)) , columns = {"Run" })
+df.insert(1, "Qlearning", numpy.cumsum(qlearning) )
+df.insert(2, "Sarsa" , numpy.cumsum(sarsa) )
+df.insert(3, "TD(0)" , numpy.cumsum(tdlearning ))
+print(df.head())
+g = (ggplot(df) +
+     geom_line(aes( x  = "Run", y = "Run"), color = "black") +
+     geom_line(aes( x  = "Run", y = "Qlearning"), color= "blue") +
+     geom_line(aes( x  = "Run", y = "Sarsa"), color = "Red") +
+     geom_line(aes( x  = "Run", y = "TD(0)"), color = "green") +
+     ggtitle("Results")
+     )
+#print(g)
+
+#save to file
+g.save(filename="results.pdf")
